452bd6887856401e74edff96c76d72b5d2b7f3b6
MartinGulla-Lightit <martin.gulla@lightit.io>
2024-11-09 16:49:34 -0300

    Add logout

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,3 +1,16 @@
+import { supabase } from '../supabase/supabaseClient';
+
 export const Home = () => {
-  return <div></div>;
+  const handleLogout = () => {
+    supabase.auth.signOut();
+  };
+
+  return (
+    <div className="w-full flex flex-col items-center">
+      <div className="w-full grid grid-cols-3 gap-4 text-center">
+        <div className="col-start-2">Welcome</div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  );
 };
